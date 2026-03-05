4f98a4369b988dfb5b5944b8a60012ca0ffe5ba1
AlienShuffle (Ubuntu@mars) <readngtndude@gmail.com>
2026-03-05 14:09:29 -0500

    tweak paths make useful.

--- scripts-bash/ps-copy-wsl.sh
+++ scripts-bash/ps-copy-wsl.sh
@@ -1,4 +1,7 @@
 # need to fix the path to the powershell script and the source and clone distro names
+REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 powershell.exe -ExecutionPolicy Bypass \
--File /mnt/c/Users/alan/Downloads/Clone-WSL2Distro.ps1 `
-  -Source "Ubuntu-24.04" -Clone "U24-lab"
+  -File "${REPO_ROOT}/scripts-powershell/Clone-WSL2Distro.ps1" \
+  -Source "Ubuntu-24.04" \
+  -Clone "U24-lab"

9dea0b7841b0782d637039669f0a2adcd5f870ba
AlienShuffle (Win11@mars) <readngtndude@gmail.com>
2026-03-05 23:58:31 -0500

    to do list for windows baseline setup.

---
+++ scripts/configure-windows.ps1
@@ -0,0 +1,7 @@
+winget install --id Microsoft.PowerShell --source winget
+# also need to install the latest .lots of stuff to work.
+# git pull the bootstrap repo.
+# install git for Windows
+# I am sure there is more.
+# pull down the latest Ubuntu image and install WSL2
+# install the latest Windows Terminal
--- scripts/install-ps7.ps1
+++
@@ -1,1 +0,0 @@
-winget install --id Microsoft.PowerShell --source winget

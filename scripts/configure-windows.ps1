069e94185e009b13885edaaa286a7884efc13834
AlienShuffle (dev@dev) <readngtndude@gmail.com>
2026-03-06 19:32:34 -0500

    logging

--- scripts/configure-windows.ps1
+++ scripts/configure-windows.ps1
@@ -1,7 +1,14 @@
 # get Powershell v7.x
+echo "###Installing PowerShell 7.x"
 winget install --id Microsoft.PowerShell --source winget
 # make sure a default instance is running.
+echo
+echo "###Installing Ubuntu instance"
 wsl.exe --install -d Ubuntu
+echo
+echo "###Updating WSL"
 wsl.exe --update
 # install Windows Git
+echo
+echo "###Installing Windows Git"
 winget install --id Git.Git -e --source winget
